d8b21853434e44fded7fd54d250ea12c5b53eaa2
exelexe <deckn.m@gmail.com>
2016-12-22 16:29:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# celler
+[Getting Started with goa](https://goa.design/learn/guide/) sample code
+
+## More good example
+[goa-cellar](https://github.com/goadesign/goa-cellar)
---
+++ bottle.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"cellar/app"
+	"fmt"
+	"github.com/goadesign/goa"
+)
+
+// BottleController implements the bottle resource.
+type BottleController struct {
+	*goa.Controller
+}
+
+// NewBottleController creates a bottle controller.
+func NewBottleController(service *goa.Service) *BottleController {
+	return &BottleController{Controller: service.NewController("BottleController")}
+}
+
+// Show implements the "show" action of the "bottles" controller.
+func (c *BottleController) Show(ctx *app.ShowBottleContext) error {
+	if ctx.BottleID == 0 {
+		// Emulate a missing record with ID 0
+		return ctx.NotFound()
+	}
+	// Build the resource using the generated data structure
+	bottle := app.GoaExampleBottle{
+		ID:   ctx.BottleID,
+		Name: fmt.Sprintf("Bottle #%d", ctx.BottleID),
+		Href: app.BottleHref(ctx.BottleID),
+	}
+
+	// Let the generated code produce the HTTP response using the
+	// media type described in the design (BottleMedia).
+	return ctx.OK(&bottle)
+}
